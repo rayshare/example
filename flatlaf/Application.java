20c3771801b4098ea1f1a75fcfd2d4d6c6c17f86
Ray <58549747+rayshare@users.noreply.github.com>
2024-08-28 19:56:55 +0800

    Add files via upload

---
+++ flatlaf/Application.java
@@ -0,0 +1,18 @@
+package org.example;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Application {
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> {
+            MainFrame frame = new MainFrame();
+            frame.setPreferredSize(new Dimension(830, 440));
+            frame.pack();
+            frame.setLocationRelativeTo(null);
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            frame.setVisible(true);
+            frame.setResizable(false);
+        });
+    }
+}
